21e1c5a7522fb05855d93b16eca5c0c6be81745f
Venel Dorzier <veneljoseph91@gmail.com>
2022-11-22 21:14:52 -0300

    commit script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function validar(){
+
+    let nome = document.forms['form']['nome'].value;
+    let email = document.forms['form']['email'].value;
+    let telefone = document.forms['form']['telefone'].value;
+    
+
+    if(nome === "" ){
+        alert('Preenche o campo nome.');
+        document.getElementById('erro').innerHTML = "<P style= 'color: #f00;'>Erro: Preenche o campo nome!</P>";
+        return false;
+    }
+
+    if(email === ""){
+        alert('Preenche o campo email.');
+        document.getElementById('erro').innerHTML ="<p style='color: #f00;'>Erro: Preenche o campo email!</p>"; 
+        return false;
+ 
+    } 
+        if(telefone === ""){
+            alert('Preenche o campo telefone.');
+            document.getElementById('erro').innerHTML = "<p style='color: #f00;'>Erro: Preenche o campo telefone!</p>";
+            return false; 
+        }
+
+        if(menssagem === ""){
+            alert('Preenche o campo menssagem.');
+            document.getElementById('erro').innerHTML = "<p style='color: #f00;'>Erro: Preenche o campo menssagem!</p>";
+            return false;
+        } 
+
+}
+
